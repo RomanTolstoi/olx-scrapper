446787fb70df5ad5f7953e43664fe32c36ddcfbd
rtolstoi <rtolstoi-ecw@cricut.com>
2025-03-25 14:43:42 +0200

    first commit

---
+++ src/test/java/edu/ntu/tolstoi/olx_scrapper/OlxScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntu.tolstoi.olx_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
